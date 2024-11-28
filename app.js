7a06f12db47e9dd406b4ad89c286dcb89a68dad6
MrHyaena <dolezalmartin131@gmail.com>
2024-11-28 17:56:55 +0100

    Templating

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+
+app.set("view engine", "ejs");
+
 // listen for requests
 
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    { title: "Yoshi", snippet: "Lorem ipsum dolor sit amet consectetur" },
+    { title: "Yoshi", snippet: "Lorem ipsum dolor sit amet consectetur" },
+    { title: "Yoshi", snippet: "Lorem ipsum dolor sit amet consectetur" },
+  ];
+  res.render("index", { title: "Home", blogs: blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create Blog" });
 });
 
 // redirects
@@ -24,5 +37,5 @@ app.get("/about-us", (req, res) => {
 // 404 Page
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
